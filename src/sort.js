61f5a7568a3545a7ff8e49f75a3e29ef7ff65eaa
Bart Froklage <bart.froklage@gmail.com>
2016-04-15 14:50:27 +0200

    Update sort.js
    new try

--- src/sort.js
+++ src/sort.js
@@ -1,4 +1,10 @@
-function sort(array) {
+function sort(cards) {
     "use strict";
-    return array;
+    
+    var VALUES = "♠♥♦♣AKQJ198765432";
+    cards.sort(function(a, b) { 
+        return (VALUES.indexOf(a[0]) * 100 + VALUES.indexOf(a[1])) - (VALUES.indexOf(b[0]) * 100 + VALUES.indexOf(b[1]));
+    });
+
+    return cards;
 }
