2fec28321ab74dae99f24ce4cee5efc698b05098
Melle Hofman <melle.hofman@nl.abnamro.com>
2016-04-15 23:59:42 +0200

    Most efficient

--- src/sort.js
+++ src/sort.js
@@ -1,14 +1,7 @@
 function sort(array) {
     "use strict";
-    var table = { A: 14, K: 13, Q: 12, J: 11, "10": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2},
-        target = {'♠': [], '♥': [], '♦': [], '♣': []},
-        sortFunction = function (a, b) {
-            return table[b.substr(1)] - table[a.substr(1)];
-        };
-
-    array.map(function (el) {
-        target[el.substr(0, 1)].push(el);
-    });
-
-    return (target['♠'].sort(sortFunction) + [','] + target['♥'].sort(sortFunction) + [','] + target['♦'].sort(sortFunction) + [','] + target['♣'].sort(sortFunction)).split(',');
+    var table = { A: 2, K: 3, Q: 4, J: 5, 10: 6, 9: 7, 8: 8, 7: 9, 6: 10, 5: 11, 4: 12, 3: 13, 2: 14, '♣': 39, '♦': 26, '♥': 13, '♠': 0}, returnArray = [];
+    return array.forEach(function (el) {
+        returnArray[table[el[0]] + table[el.substr(1)] - 2] = el;
+    }) || returnArray;
 }
