6f99bac4f8a5050813df3a4321376027b600ab09
Melle Hofman <melle.hofman@nl.abnamro.com>
2016-04-18 23:21:35 +0200

    Most robust

--- src/sort.js
+++ src/sort.js
@@ -1,7 +1,26 @@
 function sort(array) {
     "use strict";
-    var table = { A: 2, K: 3, Q: 4, J: 5, 10: 6, 9: 7, 8: 8, 7: 9, 6: 10, 5: 11, 4: 12, 3: 13, 2: 14, '♣': 39, '♦': 26, '♥': 13, '♠': 0}, returnArray = [];
-    return array.forEach(function (el) {
-        returnArray[table[el[0]] + table[el.substr(1)] - 2] = el;
-    }) || returnArray;
+    var table = { A: 2, K: 3, Q: 4, J: 5, 10: 6, 9: 7, 8: 8, 7: 9, 6: 10, 5: 11, 4: 12, 3: 13, 2: 14, '♣': 39, '♦': 26, '♥': 13, '♠': 0}, returnArray = [], i;
+    var collections = Math.ceil(array.length / 52);
+
+    function robust() {
+        for (i = 0; i < array.length; i++) {
+            var offset = 0, el = array[i];
+            while (returnArray[((table[el[0]] + table[el.substr(1)] - 2) * collections) + offset] != undefined) {
+                offset += 1;
+                if (offset >= collections - 1) {
+                    collections++;
+                    returnArray = [];
+                    return;
+                }
+
+            }
+            returnArray[((table[el[0]] + table[el.substr(1)] - 2) * collections) + offset] = el;
+        }
+    }
+
+    while (returnArray.length === 0) {
+        robust();
+    }
+    return returnArray.join(';').replace(/^;+/, '').replace(/;;+/g, ';').split(';');
 }
